39afa5c570463ca59aefdc88ee0bf467a8a0088d
Tim Riley <tim@openmonkey.com>
2009-05-27 20:33:08 +1000

    sequel migrations inline

--- coffeetime.rb
+++ coffeetime.rb
@@ -1,6 +1,19 @@
 require 'rubygems'
+require 'sequel'
 require 'sinatra'
 require 'haml'
+
+DB = Sequel.sqlite('coffeetime.db')
+
+DB.create_table :teams do
+  primary_key :id
+  String :name
+end unless DB.table_exists?(:teams)
+
+DB.create_table :subscribers do
+  Integer :team_id
+  String :twitter_username
+end unless DB.table_exists?(:subscribers)
 
 helpers do
 end
