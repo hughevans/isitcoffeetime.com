4c2794eb7c0740bf33c130ee55d7aa3c07e69e75
Hugh Evans <hugh@artpop.com.au>
2009-05-30 21:37:12 +1000

    Going silly now

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $('#byline a').click(function() {
-    $('#yes').animate({marginTop:'-40px'});
+    $('#yes').animate({marginTop:'-40px'}, function() {
+      $(this).animate({opacity:0}, function() {
+        $(this).html('custom').css('font-size', '112px').css('margin-top', '20px').animate({opacity:1});
+      });
+    });
     $('#byline').hide();
     $('#custom').slideDown('normal', function() {
       $('input#name').focus();
