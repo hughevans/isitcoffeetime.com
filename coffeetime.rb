ef6d7482c31387aec7e11a7d18599f8ef51b5b29
Shoaib <shoaib@Shoaibs-MacBook-Pro.local>
2010-07-10 18:14:38 +1000

    Swapping out sequel for mongo.

--- coffeetime.rb
+++ coffeetime.rb
@@ -1,80 +1,74 @@
-require 'rubygems'
-require 'sinatra'
-require 'yaml'
-require 'sequel'
-require 'tzinfo'
-require 'json'
-require 'haml'
-require 'httparty'
-gem 'twitter'
-require 'twitter'
-require 'twitter/httpauth'
-require 'chronic'
+require "rubygems"
+require "sinatra"
+require "yaml"
+require "active_model"
+require "mongoid"
+require "tzinfo"
+require "json"
+require "haml"
+# require "httparty"
+gem "twitter"
+require "twitter"
+require "twitter/httpauth"
+require "chronic"
 
-configure do
-  DB     = Sequel.sqlite('db.sqlite3')
-  CONFIG = YAML.load(File.read 'config.yml')
+configure do  
+  Mongoid::Config.instance.from_hash(YAML.load(File.read("config/database.yml")))
+  CONFIG = YAML.load(File.read("config/config.yml"))
 end
-
-DB.create_table :teams do
-  primary_key :id
-
-  String :name
-  String :time_zone
-  String :twitter_account
-end unless DB.table_exists?(:teams)
-
 
 module TwitterSession
   def twitter_session
     @twitter_session ||= Twitter::Base.new(twitter_auth)
   end
-  
+
   def twitter_auth
     @twitter_auth ||= Twitter::HTTPAuth.new(
-        CONFIG['twitter']['username'],
-        CONFIG['twitter']['password']
+        CONFIG["twitter"]["username"],
+        CONFIG["twitter"]["password"]
       )
   end
 end
 
-class Team < Sequel::Model
-  plugin :validation_helpers
+class Team
+  include Mongoid::Document
+
+  field :name
+  field :time_zone
+  field :twitter_account
+
+  validates_presence_of :name, :time_zone, :twitter_account
+
+  validates_format_of :name,
+    :with    => /^[A-Za-z0-9]+[A-Za-z0-9\-_]*$/, 
+    :message => "Can only include a-z, dashes and unerscores"
+
+  validates_uniqueness_of :name, :message => "Not available"
 
   def validate
-    validates_presence [:name, :time_zone, :twitter_account],
-      :message => "Can't be blank"
-
-    validates_format /^[A-Za-z0-9]+[A-Za-z0-9\-_]*$/,
-      :name,
-      :message => 'Can only include a-z, dashes and unerscores'
-
-    validates_unique :name,
-      :message => 'Not available'
-
     if errors.empty? && Twitter.user(self.twitter_account).error?
-      errors[:twitter_account] << 'Does not exist'
+      errors[:twitter_account] << "Does not exist"
     end
   end
-  
+
   def messages
     # @messages ||= DirectMessage.messages_for(self.twitter_account)
     DirectMessage.messages_for(self.twitter_account)
   end
-  
+
   def coffee_times
     messages.map { |msg|
       time = Chronic.parse(msg.text)
       time..(time + 20*60) if time
     }.compact
   end
-  
+
   def coffee_time_now?
     coffee_times.any? {|d| d.include?(Time.now)}
   end
-  
+
   include TwitterSession
-  
+
   def after_save
     begin
       twitter_session.friendship_create(self.twitter_account)
@@ -92,7 +86,7 @@ class DirectMessage
     @created_at = attributes[:created_at]
     @text       = attributes[:text]
   end
-  
+
   self.extend(TwitterSession)
 
   def self.get_messages
@@ -104,7 +98,7 @@ class DirectMessage
         )
     end
   end
-  
+
   def self.messages_for(user, since=24*60)
     get_messages.select do |msg|
       msg.username == user && msg.created_at > (Time.now.utc - (since*60))
@@ -117,28 +111,28 @@ helpers do
 
   def random_quip
     quips = [
-        'it sure is...',
-        "and don't you know it...",
-        'finally!',
-        'go get it!',
-        'why not try a ristretto?',
-        'emphatically',
-        'so hit up <a href="http://decafsucks.com">decafsucks.com</a>'
-      ]
+      "it sure is...",
+      "and don’t you know it...",
+      "finally!",
+      "go get it!",
+      "why not try a ristretto?",
+      "emphatically",
+      "so hit up <a href=\"http://decafsucks.com\">decafsucks.com</a>"
+    ]
     quips[rand(quips.size)]
   end
 end
 
-get '/' do
+get "/" do
   haml :home
 end
 
-get '/:style.css' do
-  content_type 'text/css', :charset => 'utf-8'
+get "/:style.css" do
+  content_type "text/css", :charset => "utf-8"
   sass :"stylesheets/#{params[:style]}"
 end
 
-post '/teams' do
+post "/teams" do
   content_type :json
   team = Team.new(params[:team])
   if attribute = params[:validate]
@@ -154,14 +148,15 @@ post '/teams' do
       team.save
       status 201
       {:success => true, :team => {:name => team.name}}.to_json
-    rescue Sequel::ValidationFailed
+    rescue # TODO: mongoid exception failed save
       status 200
       {:success => false, :errors => team.errors}.to_json
     end
   end
 end
 
-get '/:team_name' do
-  @team = Team[:name => params[:team_name]] || raise(Sinatra::NotFound)
+get "/:team_name" do
+  @team = Team.find(:first, :conditions => {:name => params[:team_name]})
+  # raise(Sinatra::NotFound)
   haml :team
 end
